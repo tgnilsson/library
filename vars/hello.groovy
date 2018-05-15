56c7baa49de4454c64f5c1e7d22ef45934aabefd
Tanner Nilsson <tnilsson@artemishealth.com>
2018-05-15 16:44:25 -0600

    fix typo

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -2,7 +2,7 @@
 def call(options) {
 	sh 'echo Hello World'
 //consider using camelCase for groovy and all caps for env
-	dev results = sh(script:"echo ${options}", returnStdout:true).trim()
+	def results = sh(script:"echo ${options}", returnStdout:true).trim()
 	echo "${results}"
 	def varHere = "Something else"
 	echo "${varHere}"
